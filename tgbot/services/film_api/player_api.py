a3d562c374634e64f06544920fcb17984a6fb93c
Ninnjah <yand.man4@gmail.com>
2023-10-03 13:14:09 +0300

    Add exception handlers Add _request_get method

--- tgbot/services/film_api/player_api.py
+++ tgbot/services/film_api/player_api.py
@@ -1,14 +1,18 @@
 import asyncio
-import re
 import json
+import re
+import logging
 from typing import Optional, List
 
 from aiocache import cached, RedisCache
 from aiocache.serializers import PickleSerializer
-from httpx import AsyncClient
+from httpx import AsyncClient, ConnectTimeout, ConnectError, ReadTimeout, Response
 
 from tgbot.services.film_api.models.films import Film
 from tgbot.services.film_api.models.player import Season, Episode, Source
+
+logger = logging.getLogger(__name__)
+TIMEOUT = 3
 
 
 class PlayerAPI:
@@ -17,6 +21,19 @@ class PlayerAPI:
 
         self._base_url = f"https://api.bhcesh.me"
         self._franchise_details = f"{self._base_url}/franchise/details"
+
+    @staticmethod
+    @cached(ttl=3600, cache=RedisCache, serializer=PickleSerializer(), namespace="cache")
+    async def _request_get(
+            client: AsyncClient, url: str, params: Optional[dict] = None, timeout: int = TIMEOUT, attempts: int = 5
+    ) -> Optional[Response]:
+        for _ in range(attempts):
+            try:
+                return await client.get(url, params=params)
+            except (ReadTimeout, ConnectTimeout, ConnectError) as e:
+                logger.warning(f"Exception {e} on {url}\nWaiting of {TIMEOUT} seconds", exc_info=True)
+                await asyncio.sleep(timeout)
+                continue
 
     @staticmethod
     async def _parse_iframe_url(raw_data: dict) -> Optional[str]:
@@ -54,56 +71,63 @@ class PlayerAPI:
                 return Source(film_id=kinopoisk_id, url=data)
 
     @cached(ttl=30, cache=RedisCache, serializer=PickleSerializer(), namespace="cache")
-    async def get_film_data(self, kinopoisk_id: int) -> Optional[str]:
+    async def get_film_url(self, kinopoisk_id: int) -> Optional[str]:
         params = {
             "token": self.token,
             "kinopoisk_id": kinopoisk_id
         }
 
         async with AsyncClient() as client:
-            res = await client.get(self._franchise_details, params=params)
+            res = await self._request_get(client, self._franchise_details, params=params)
             if res.status_code == 200:
                 return await self._parse_iframe_url(res.json())
 
     @cached(ttl=30, cache=RedisCache, serializer=PickleSerializer(), namespace="cache")
     async def get_film_source(self, kinopoisk_id: int) -> Optional[Source]:
-        link = await self.get_film_data(kinopoisk_id)
+        link = await self.get_film_url(kinopoisk_id)
         if not link:
             return
 
         async with AsyncClient() as client:
-            res = await client.get(link)
+            res = await self._request_get(client, link)
             return await self._parse_source(kinopoisk_id, res.text)
 
     @cached(ttl=3600, cache=RedisCache, serializer=PickleSerializer(), namespace="cache")
     async def check_available(self, films: List[Film]) -> List[Source]:
-        async with AsyncClient() as client:
-            r = await asyncio.gather(
-                *[
-                    client.get(
-                        self._franchise_details,
-                        params={
-                            "token": self.token,
-                            "kinopoisk_id": film.film_id
-                        }
-                    )
-                    for film in films
+        try:
+            async with AsyncClient() as client:
+                r = await asyncio.gather(
+                    *[
+                        self._request_get(
+                            client,
+                            self._franchise_details,
+                            params={
+                                "token": self.token,
+                                "kinopoisk_id": film.film_id
+                            }
+                        )
+                        for film in films
+                    ]
+                )
+                links = [
+                    (x.request.url.params["kinopoisk_id"], await self._parse_iframe_url(x.json()))
+                    for x in r if x.status_code == 200
                 ]
-            )
-            links = [
-                (x.request.url.params["kinopoisk_id"], await self._parse_iframe_url(x.json()))
-                for x in r if x.status_code == 200
-            ]
 
-            r = await asyncio.gather(
-                *[
-                    client.get(link[1], params={"kinopoisk_id": link[0]})
-                    for link in links
+                r = await asyncio.gather(
+                    *[
+                        self._request_get(client, link[1], params={"kinopoisk_id": link[0]})
+                        for link in links if link[1]
+                    ]
+                )
+                sources = [
+                    await self._parse_source(x.request.url.params["kinopoisk_id"], x.text)
+                    for x in r if x.status_code == 200
                 ]
-            )
-            sources = [
-                await self._parse_source(x.request.url.params["kinopoisk_id"], x.text)
-                for x in r if x.status_code == 200
-            ]
+
+        except (ReadTimeout, ConnectTimeout, ConnectError) as e:
+            logger.warning(f"{e}\nWaiting of {TIMEOUT} seconds", exc_info=True)
+            await asyncio.sleep(TIMEOUT)
+            return await self.check_available(films)
 
         return sources
